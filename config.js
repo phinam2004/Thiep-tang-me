6335212196f560d983b12e6c1888b9d63533b82b
phinam2004 <126351620+phinam2004@users.noreply.github.com>
2023-03-04 00:33:31 +0700

    Thieptangme

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mẹ';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100088074202121';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng Mẹ Của Con';
+const contentCard = '8-3 sắp đến rồi mà con vẫn chưa có gì để tặng cho mẹ cả. 
+                    Mẹ là người quan trọng nhất trong cuộc đời của con. 
+                    Mẹ là người đã che chở cho con, những lúc con buồn, mẹ luôn an ủi cho con… 
+                    Nhưng con chưa bao giờ tặng một món quà gì cho mẹ. 
+                    Con chúc Mẹ của con mỗi ngày đều vui vẻ và sống khỏe, con yêu Mẹ nhiều.💘';
+
+const giftImage = 'me.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
